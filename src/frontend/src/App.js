8ab9fb120ea8b33f9d4391f9386a27e1fd6fe9b6
NicoACloutier <nicocloutier1@gmail.com>
2025-06-02 11:16:20 -0400

    add basic infrastructure for routing

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './components/Home.js';
+
+const SERVER_PORT = 3000;
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [userData, setUserData] = useState({});
+    const [loggedIn, setLoggedIn] = useState(false);
+    
+    const router = createHashRouter([
+        {
+            path: '/',
+            element: <Home />,
+        },
+    ]);
+
+    return (
+        <div className="app-container">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createHash } from 'crypto';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />);
